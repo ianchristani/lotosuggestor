f30954ba384902689504b4b00ddeb54e2db1023a
ianchristani <ianchristani@gmail.com>
2021-11-11 22:23:26 -0300

    correcting vulnerability

--- lotosuggest/lotosugest/settings.py
+++ lotosuggest/lotosugest/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qa$$(o5jx+mb2o3+lt)(1s08z4(=p3+1e(s1wegywq3ji$m5&l'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
