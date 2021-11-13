dda22d75a6f6a00ac4a7432ba9341a8419e07f12
ianchristani <ianchristani@gmail.com>
2021-11-13 16:12:56 -0300

    starting new version - general one

--- lotosuggest/Loto/views.py
+++ lotosuggest/Loto/views.py
@@ -2,29 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from random import randint
 
-# Create your views here.
-
-
 def loto(request):
     lista = []
     cont=0
     if request.method == 'POST':
-        opcao = request.POST.get('Loto',False)
-        if opcao == "Loto":
-            while cont <= 5:
-                n = randint(1,61)
-                if n not in lista:
-                    lista.append(n)
-                    cont += 1
-            resultado = lista
-        else:
-            while cont < 15:
-                n = randint(1,26)
-                if n not in lista:
-                    lista.append(n)
-                    cont += 1
-            resultado = lista
-
+        range_ = int(request.POST.get('range',False))
+        amount = int(request.POST.get('amount',False))
+        while cont <= amount:
+            n = randint(1,range_+1)
+            if n not in lista:
+                lista.append(n)
+                cont += 1
+        resultado = lista
         return render(request,'loto.html',{'resultado':resultado})
     else:
         return render(request,'loto.html')
